70605c59ed8adcdc7320decfd1c6720935c95f06
SergeMaikere <serge.maikere@gmail.com>
2025-11-02 22:16:11 +0100

    + background display + player display + meteor display + laser display

--- main.py
+++ main.py
@@ -1,39 +1,73 @@
 import pygame
-from numpy import random
-import modules.Loader
+from modules.Loader import image_loader
+from modules.Helper import set_rect, add_to_surface, pipe, curry, get_random_pos, is_out_of_bound
 
 # General setup
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
-running = True
 
 # Utils functions
-quit_game = lambda pygame, event: False if event.type == pygame.QUIT else True 
-get_random_pos = lambda: ( random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT) )
+def setScreen ( w, h ):
+	screen = pygame.display.set_mode((w, h))
+	pygame.display.set_caption('Space Shooter I - Revenge Of The Bit')	
+	return screen
 
-def setBackground ( screen ):
+def setBackground ( screen, positions ):
 	screen.fill('darkgray')
-	star = modules.Loader.image_loader('star.png')
+	star = image_loader('star.png')
 	for i in range(20):
-		screen.blit(star, get_random_pos())
+		screen.blit(star, positions[i])
+
+def set_game_obj (anchor, pos, src):
+	return pipe(
+		[
+			image_loader,
+			curry(set_rect)(anchor, pos),
+		]
+	)(src)
+
+def move_player ( width, direction, player ):
+	player["rect"].x += direction 
+	return player
 
 
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+screen = { "width": WINDOW_WIDTH, "height": WINDOW_HEIGHT, "surface": None }
 
 pygame.init()
 
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('Space Shooter I - Revenge Of The Bit')
+screen["surface"] = setScreen(screen['width'], screen['height'])
+positions = [ get_random_pos(screen["width"], screen["height"]) for i in range(20) ]
+add_to_screen = curry(add_to_surface)(screen["surface"])
 
+player = set_game_obj('midleft', (0, screen["height"]/2), 'player.png')
+meteor = set_game_obj('center', (screen["width"]/2, screen["height"]/2), 'meteor.png')
+laser = set_game_obj('bottomleft', (20, screen["height"] - 20), 'laser.png')
+
+direction = 1
+running = True
 while running:
 	# Event loop
 	for event in pygame.event.get():
-		running = quit_game(pygame, event)
+		running = not event.type == pygame.QUIT
 
-	# Draw game
-	setBackground(screen)
+	# Directing direction
+	direction *= ( -1 if is_out_of_bound(player["rect"], screen["width"]) else 1 )
+
+	# Background
+	setBackground(screen["surface"], positions)
+	
+	# Add game objs to screen
+	add_to_screen(meteor)
+	add_to_screen(laser)	
+	pipe( 
+		[ 
+			curry(move_player)(screen["width"], direction),
+			add_to_screen
+		] 
+	)(player)
 	
 	pygame.display.update()
-
 
 pygame.quit()
--- modules/Loader.py
+++ modules/Loader.py
@@ -1,7 +1,8 @@
 import pygame
 from os.path import join
-import modules.Helper
+from modules.Helper import pipe
 
 load_image = lambda name: pygame.image.load( join('assets', 'images', name) )
-convert_image = lambda surface: surface.convert_alpha()
-image_loader = modules.Helper.pipe([load_image, convert_image])
+convert_image = lambda surface, alpha=True: surface.convert_alpha() if alpha else surface.convert()
+image_loader = pipe([load_image, convert_image])
+
